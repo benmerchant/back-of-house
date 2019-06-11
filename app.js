c027a6d9b76f1b45a0b8e4d5aa02c7b8098bb92a
Ben Merchant <benjaminmerchant@gmail.com>
2019-06-11 01:02:31 -0400

    route GET requests to /api/[linkLister,stories]. separate concerns

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-const express = require('express');
+/*
+ * Title: ~/app.js
+ *
+ * Description: 1. ExpressJS object instantiation
+ *              2. API initialization
+ *
+ * Author: Ben Merchant
+*/
 
+const express = require('express');
+// import API routes
+const API_Router = require('./components/router');
 console.log('app (ExpressJS) is starting up...');
 
 const app = express();
@@ -7,14 +17,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-// hard code routes for now import later
-app.get('/', (req, res) => {
-  res.send('Heyooooo World');
-});
+// import ALL routes here
+API_Router(app);
+
 // handle 404s
 app.use((req, res, next) => {
   res.status(404);
   res.json('404 - error');
 });
+
 // export the app object
 module.exports = app;
